a07cca38c1c26827584c3b1ac07042509b97f344
Breno Rosa <71451784+Akunozz@users.noreply.github.com>
2025-04-18 12:08:08 -0300

    criação do projeto e estrutura

---
+++ flash-card/src/components/app-sidebar.tsx
@@ -0,0 +1,103 @@
+"use client"
+
+import * as React from "react"
+import {
+  Frame,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "Breno",
+    email: "breno@email.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Cards",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Criar card",
+          url: "#",
+        },
+        {
+          title: "Estudar cards",
+          url: "#",
+        },
+        {
+          title: "Fazer prova",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Configurações",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "Geral",
+          url: "#",
+        },
+        {
+          title: "Cards",
+          url: "#",
+        },
+        {
+          title: "Perfil",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: "Design Engineering",
+      url: "#",
+      icon: Frame,
+    },
+    {
+      name: "Sales & Marketing",
+      url: "#",
+      icon: PieChart,
+    },
+    {
+      name: "Travel",
+      url: "#",
+      icon: Map,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader className="flex items-center">
+        Flash Card
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
